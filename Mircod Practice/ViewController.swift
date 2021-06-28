cd4cdaf691618ed227e285e897b8cf3ebc821c75
Alsu Giniatullina <g.alsu.z.212@gmail.com>
2021-06-28 16:43:50 +0300

    Create project

---
+++ Mircod Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mircod Practice
+//
+//  Created by Алсу Гиниятуллина  on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
